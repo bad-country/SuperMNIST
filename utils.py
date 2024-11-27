2b4ec9f26642afc9c209ce41df46f6ab1b06dec7
Charles Fisher <charleskennethfisher@gmail.com>
2024-11-27 13:57:52 -0800

    fix mnist file types, update utils.py

--- utils.py
+++ utils.py
@@ -1,22 +1,48 @@
-def load_mnist(path, kind='train'):
-    import os
-    import gzip
-    import numpy as np
+import os
+import gzip
+import numpy as np
 
-    """Load MNIST data from `path`"""
-    labels_path = os.path.join(path,
-                               '%s-labels-idx1-ubyte.gz'
-                               % kind)
-    images_path = os.path.join(path,
-                               '%s-images-idx3-ubyte.gz'
-                               % kind)
+# Get the directory containing the current module
+FILEDIR = os.path.dirname(os.path.abspath(__file__))
+
+def load(dataset='digits', split='train'):
+    """
+    Load one of the MNIST-style datasets.
+
+    Args:
+        dataset (str): The dataset to load. Must be one of 'digits', 'fashion', 'letters', 'super'.
+        split (str): The split to load. Must be one of 'train', 'test'.
+
+    Returns:
+        tuple: A tuple containing the images and labels.
+    """
+    assert dataset in ['digits', 'fashion', 'letters', 'super'], \
+        "Dataset must be one of 'digits', 'fashion', 'letters', 'super'"
+    
+    assert split in ['train', 'test'], \
+        "Split must be one of 'train', 'test'"
+    
+    labels_path = os.path.join(
+        FILEDIR, dataset, f"{dataset}-{split}-labels-idx1-ubyte.gz"
+    )
+
+    images_path = os.path.join(
+        FILEDIR, dataset, f"{dataset}-{split}-images-idx3-ubyte.gz"
+    )
 
     with gzip.open(labels_path, 'rb') as lbpath:
-        labels = np.frombuffer(lbpath.read(), dtype=np.uint8,
-                               offset=8)
+        labels = np.frombuffer(
+            lbpath.read(), dtype=np.uint8, offset=8
+        )
 
     with gzip.open(images_path, 'rb') as imgpath:
-        images = np.frombuffer(imgpath.read(), dtype=np.uint8,
-                               offset=16).reshape(len(labels), 784)
+        images = np.frombuffer(
+            imgpath.read(), dtype=np.uint8, offset=16
+        ).reshape(len(labels), 784)
 
     return images, labels
+
+
+if __name__ == "__main__":
+    imgs, lbs = load(dataset='digits', split='train')
+    print(imgs.shape, lbs.shape)
